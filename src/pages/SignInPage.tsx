6fd704ccdec35696250342b8ebb18a07fa121a7e
Ben Djibril <konedjibril2004@gmail.com>
2025-10-02 14:15:54 +0100

    Update index.html and SignInPage for branding and code consistency
    - Changed the favicon from SVG to PNG format and added a shortcut icon for better branding.
    - Updated the page title to "MIMAA - Suivi de Grossesse" for improved clarity.
    - Adjusted indentation in SignInPage for better code consistency.

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -190,7 +190,7 @@ const SignInPage: React.FC = () => {
               Adresse email
             </label>
             <div className="relative">
-              <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
+               <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
                 <Mail className={`h-5 w-5 ${fieldErrors.email ? 'text-red-400' : 'text-gray-400'}`} />
               </div>
               <input
